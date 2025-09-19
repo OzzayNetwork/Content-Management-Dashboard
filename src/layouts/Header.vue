b504c42b9f173d6144b32c9a47782472105fc767
Kelvin Njuguna <ozzaynetwork@gmail.com>
2025-09-19 11:47:46 +0300

    kelvin: the AI agent was separated

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -78,73 +78,7 @@
                 </div>
 
 
-               <div class="dropdown d-inline-block d-lg-inline-block ms-1">
-  <button type="button" class="btn header-item noti-icon waves-effect" data-bs-toggle="dropdown"
-    aria-haspopup="true" aria-expanded="false">
-    <i class="bx bx-customize"></i>
-  </button>
-  <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-    <div class="dropdown-header fw-bold text-center border-bottom pb-3">
-      <h6 class="text-black mb-0">CS AI Agents Suit</h6>
-    </div>
-    <div class="px-lg-2 pt-2">
-      <div class="row g-0">
-        <div class="col-4">
-          <a class="dropdown-icon-item p-2" href="#"
-             data-bs-toggle="tooltip" data-bs-placement="top"
-             title="Access CSP AI Agent">
-            <img src="../assets/images/AI-agent/favicon.png" alt="CSP Agent">
-            <span>CSP Agent</span>
-          </a>
-        </div>
-        <div class="col-4">
-          <a class="dropdown-icon-item p-2" href="#"
-             data-bs-toggle="tooltip" data-bs-placement="top"
-             title="AI Agent for Homa Bay Executive">
-            <img src="../assets/images/AI-agent/homa-bay.png" alt="Homa Bay Executive">
-            <span>Homa Bay Executive</span>
-          </a>
-        </div>
-        <div class="col-4">
-          <a class="dropdown-icon-item p-2" href="#"
-             data-bs-toggle="tooltip" data-bs-placement="top"
-             title="AI Agent for Meru Executive">
-            <img src="../assets/images/AI-agent/county_logo.png" alt="Meru Executive">
-            <span>Meru Executive</span>
-          </a>
-        </div>
-      </div>
-
-      <div class="row g-0">
-        <div class="col-4">
-          <a class="dropdown-icon-item p-2" href="#"
-             data-bs-toggle="tooltip" data-bs-placement="top"
-             title="KPLC AI Agent">
-            <img src="../assets/images/AI-agent/power.svg" alt="KPLC">
-            <span>KPLC</span>
-          </a>
-        </div>
-        <div class="col-4">
-          <a class="dropdown-icon-item p-2" href="#"
-             data-bs-toggle="tooltip" data-bs-placement="top"
-             title="AI Agent for Homa Bay Citizen">
-            <img src="../assets/images/AI-agent/homa-bay.png" alt="Homa Bay Citizen">
-            <span>Homa Bay Citizen</span>
-          </a>
-        </div>
-        <div class="col-4">
-          <a class="dropdown-icon-item p-2" href="#"
-             data-bs-toggle="tooltip" data-bs-placement="top"
-             title="AI Agent for Meru Citizen">
-            <img src="../assets/images/AI-agent/county_logo.png" alt="Meru Citizen">
-            <span>Meru Citizen</span>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
+             
 
 
 
